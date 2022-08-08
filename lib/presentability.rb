10eb8ee8da3d40210e2dc2f99f979064ac3f18f8
Michael Granger <ged@FaerieMUD.org>
2022-08-07 19:46:58 -0700

    Initial commit.

---
+++ lib/presentability.rb
@@ -0,0 +1,15 @@
+# -*- ruby -*-
+# frozen_string_literal: true
+
+# Toplevel namespace
+module Presentability
+
+	# Package version
+	VERSION = '0.0.1'
+
+	# Version control revision
+	REVISION = %q$Revision$
+
+
+end # module Presentability
+
---
+++ spec/presentability_spec.rb
@@ -0,0 +1,14 @@
+# -*- ruby -*-
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+require 'rspec'
+require 'presentability'
+
+RSpec.describe Presentability do
+
+	
+
+end
+
